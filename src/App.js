8b8285f4f343dbcc63101322007fa18b1332bf44
Ravi Asthana <ravi@brainstacktechnologies.com>
2021-10-20 17:28:46 +0530

    initial commit: create template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const products = [
+  {
+    id: 1,
+    name: "Coffee Mug",
+    price: 70,
+  },
+  {
+    id: 2,
+    name: "Box",
+    price: 10,
+  },
+  {
+    id: 3,
+    name: "Bottol",
+    price: 40,
+  },
+];
+
+class App extends Component {
+  state = {
+    items: products,
+  };
+
+  render() {
+    return (
+      <div>
+        <section>Cart Items - 0</section>          
+        <section>Total Cart Price - 0</section> 
+        <table>
+          <thead>
+            <tr>
+              <th>Items</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>
+                  <button>+</button>
+                  <span>0</span>
+                  <button>-</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
